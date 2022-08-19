fabe722923ce985990c7937aa6ea68e2709cfbcb
suvorova-ma <suvorova-ma@groupbwt.com>
2022-08-19 12:48:03 +0300

    fix: search

--- resources/js/components/MainPage.vue
+++ resources/js/components/MainPage.vue
@@ -1,60 +1,130 @@
 <template>
     <v-app>
-        <auth style="height: 80px"></auth>
-        <Slide right style=" z-index: 100; position: relative; top: -100px;" width="550" v-if="isAuth()">
-            <div v-for="name in categories" :value="name" :key="name.id">
-                <v-checkbox v-model="formData.parentCategory" :label="name" :value="name"
-                    @change="count++; getConferences()">
-                </v-checkbox>
-            </div>
-            <v-col cols="12" sm="10" md="26">
-                <v-menu ref="menu" v-model="menu" :close-on-content-click="false" :return-value.sync="date"
-                    transition="scale-transition" offset-y min-width="auto">
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-text-field v-model="date" label="Date from" prepend-icon="mdi-calendar" readonly
-                            v-bind="attrs" v-on="on"></v-text-field>
-                    </template>
-                    <v-date-picker v-model="date" no-title scrollable>
-                        <v-spacer></v-spacer>
-                        <v-btn text color="primary" @click="menu = false">
-                            Cancel
-                        </v-btn>
-                        <v-btn text color="primary" @click="$refs.menu.save(date); count++; getConferences()">
-                            OK
-                        </v-btn>
-                    </v-date-picker>
-                </v-menu>
-            </v-col>
-            <v-col cols="12" sm="10" md="26">
-                <v-menu ref="menu2" v-model="menu2" :close-on-content-click="false" :return-value.sync="date2"
-                    transition="scale-transition" offset-y min-width="auto">
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-text-field v-model="date2" label="Date to" prepend-icon="mdi-calendar" readonly
-                            v-bind="attrs" v-on="on"></v-text-field>
-                    </template>
-                    <v-date-picker v-model="date2" no-title scrollable>
-                        <v-spacer></v-spacer>
-                        <v-btn text color="primary" @click="menu2 = false">
-                            Cancel
-                        </v-btn>
-                        <v-btn text color="primary" @click="$refs.menu2.save(date2); count++; getConferences()">
-                            OK
-                        </v-btn>
-                    </v-date-picker>
-                </v-menu>
-            </v-col>
-            <v-col cols="12">
-                <v-slider v-model="numberOfReports" :thumb-size="24" thumb-label="always"
-                    @change="count++; number++; getConferences()" max="10"></v-slider>
-            </v-col>
+        <v-row class="shrink">
             <v-col>
-                <v-btn x-big depressed color="error" @click="resetFilters">Reset</v-btn>
+                <auth style="height: 80px; width: 800px"></auth>
             </v-col>
-        </Slide>
-        
+            <v-col align="right" style=" z-index: 100; position: relative; height: fit-content; top: -3px;">
+                <v-text-field @click="toggleMarker" @click:append="toggleMarker" clickable solo color="white"
+                    placeholder="Search..." :append-icon="marker ? 'mdi-map-marker' : 'mdi-map-marker-off'">
+                </v-text-field>
+            </v-col>
+            <v-col align="right" style=" z-index: 100; height: 94px; width: 30px; top: -20px; position: relative;">
+                <Slide right width="550" v-if="isAuth()">
+                    <div v-for="name in categories" :value="name" :key="name.id">
+                        <v-checkbox v-model="formData.parentCategory" :label="name" :value="name"
+                            @change="count++; getConferences()">
+                        </v-checkbox>
+                    </div>
+                    <v-row>
+                        <v-col cols="4" sm="4" md="6">
+                            <v-menu ref="menu" v-model="menu" :close-on-content-click="false" :return-value.sync="date"
+                                transition="scale-transition" offset-y min-width="auto">
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-text-field v-model="date" label="Date from" prepend-icon="mdi-calendar" readonly
+                                        v-bind="attrs" v-on="on"></v-text-field>
+                                </template>
+                                <v-date-picker v-model="date" no-title scrollable>
+                                    <v-spacer></v-spacer>
+                                    <v-btn text color="primary" @click="menu = false">
+                                        Cancel
+                                    </v-btn>
+                                    <v-btn text color="primary"
+                                        @click="$refs.menu.save(date); count++; getConferences()">
+                                        OK
+                                    </v-btn>
+                                </v-date-picker>
+                            </v-menu>
+                        </v-col>
+                        <v-col cols="12" sm="10" md="26">
+                            <v-menu ref="menu2" v-model="menu2" :close-on-content-click="false"
+                                :return-value.sync="date2" transition="scale-transition" offset-y min-width="auto">
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-text-field v-model="date2" label="Date to" prepend-icon="mdi-calendar" readonly
+                                        v-bind="attrs" v-on="on"></v-text-field>
+                                </template>
+                                <v-date-picker v-model="date2" no-title scrollable>
+                                    <v-spacer></v-spacer>
+                                    <v-btn text color="primary" @click="menu2 = false">
+                                        Cancel
+                                    </v-btn>
+                                    <v-btn text color="primary"
+                                        @click="$refs.menu2.save(date2); count++; getConferences()">
+                                        OK
+                                    </v-btn>
+                                </v-date-picker>
+                            </v-menu>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-slider v-model="numberOfReports" :thumb-size="24" thumb-label="always"
+                                @change="count++; number++; getConferences()" max="10"></v-slider>
+                        </v-col>
+                        <v-col>
+                            <v-btn x-big depressed color="error" @click="resetFilters">Reset</v-btn>
+                        </v-col>
+                    </v-row>
+                </Slide>
+            </v-col>
+        </v-row>
+        <v-card v-if="menuOpen">
+            <v-card-title>
+                <v-text-field label="Search" single-line solo style="padding-right: 20px" @change="getSearch()"
+                    v-model="title"></v-text-field>
+                <v-divider :vertical="true" color="black" class="mx-2"></v-divider>
+                <v-radio-group v-model="category" @change="getSearch()"
+                    style="width: 400px; padding-left: 20px; padding-bottom: 20px;">
+                    <v-radio v-for="name in tools" :key="name" :label="name" :value="name"></v-radio>
+                </v-radio-group>
+            </v-card-title>
+            <v-card-text>
+                <div class="row justify-content-center">
+                    <div class="col-md-12" v-for="conference in conferences" :value="conference.id"
+                        :key="conference.id">
+                        <v-card elevation="3">
+                            <v-card-title>{{ conference.title }}</v-card-title>
+                            <v-card-text>
+                                Appointed time: {{ conference.date }} {{ conference.time }}
+                                <br>
+                                <span>
+                                    <v-btn x-big depressed color="success">
+                                        <router-link class="white--text" style="text-decoration: none; color: inherit;"
+                                            :to="{ name: 'ConferenceDetails', params: { id: conference.id } }"
+                                            target="_blank">
+                                            More
+                                        </router-link>
+                                    </v-btn>
+                                </span>
+                            </v-card-text>
+                            Conference
+                        </v-card>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-12" v-for="report in reports" :value="report.id" :key="report.id">
+                        <v-card elevation="3">
+                            <v-card-title>{{ report.thema }}</v-card-title>
+                            <v-card-text>
+                                Duration: {{ report.start_time }} to {{ report.end_time }}
+                                <br>
+                                <span>
+                                    <v-btn x-big depressed color="success">
+                                        <router-link class="white--text" style="text-decoration: none; color: inherit;"
+                                            :to="{ name: 'Details', params: { id: report.conference_id, rep_id: report.id } }"
+                                            target="_blank">
+                                            More
+                                        </router-link>
+                                    </v-btn>
+                                </span>
+                            </v-card-text>
+                            Report
+                        </v-card>
+                    </div>
+                </div>
+            </v-card-text>
+        </v-card>
         <div class="row justify-content-center">
-            <skeleton v-if="sortedProducts.length == 0"></skeleton>
-            <div class="col-md-8" v-for="conference in sortedProducts" :value="conference.id" :key="conference.id">
+            <skeleton v-if="sortedProducts.length == 0 && search == 0 "></skeleton>
+            <div class="col-md-12" v-for="conference in sortedProducts" :value="conference.id" :key="conference.id">
                 <v-card elevation="3">
                     <v-card-title>{{ conference.title }}</v-card-title>
                     <v-card-text>
@@ -80,7 +150,7 @@
                         </span>
                     </v-card-text>
                 </v-card>
-            </div>   
+            </div>
             <Pagination :data="data" @pagination-change-page="getConferences" />
         </div>
     </v-app>
@@ -104,7 +174,16 @@ export default {
         menu2: false,
         numberOfReports: null,
         count: 0,
-        number: 0
+        number: 0,
+        menuOpen: false,
+        disable: false,
+        marker: true,
+        tools: ['Report', 'Conference'],
+        category: '',
+        conferences: [],
+        reports: [],
+        title: '',
+        search: 0
     }),
     mounted() {
         console.log(this.sortedProducts)
@@ -141,7 +220,7 @@ export default {
         getConferences(page = 1) {
             if (this.count == 0) {
                 this.sortedProducts = []
-                if(this.number == 0){
+                if (this.number == 0) {
                     this.numberOfReports = ""
                 }
                 this.getAllConferences([page, this.numberOfReports, this.cats, this.date, this.date2])
@@ -154,7 +233,7 @@ export default {
                     })
                 })
                 this.cats = [...new Set(this.cats)]
-                if(this.number == 0){
+                if (this.number == 0) {
                     this.numberOfReports = ""
                 }
                 this.$store.dispatch("ajaxConferences", [page, this.numberOfReports, this.cats, this.date, this.date2]).then(() => {
@@ -181,6 +260,48 @@ export default {
         },
         resetFilters() {
             this.$router.go()
+        },
+        toggleMarker() {
+            this.menuOpen = !this.menuOpen
+            this.marker = !this.marker
+        },
+        getSearch() {
+            this.search ++
+            if (this.title != '') {
+                this.sortedProducts = []
+                if (this.category == 'Conference') {
+                    this.getConfByName()
+                }
+                else if (this.category == 'Report') {
+                    this.getRepByName()
+                }
+                else {
+                    this.getConfByName()
+                    this.getRepByName()
+                }
+            }
+        else{
+            this.conferences = []
+            this.reports = []
+        }
+        },
+        getConfByName(page = 1) {
+            this.conferences = []
+            this.reports = []
+            this.$store.dispatch('ajaxGetConferenceByName', [page, this.title]).then(() => {
+                this.$store.getters.getConferences.data.forEach(element => {
+                    this.conferences.push(element);
+                })
+            })
+        },
+        getRepByName(page = 1) {
+            this.conferences = []
+            this.reports = []
+            this.$store.dispatch('ajaxGetReportsByName', [page, this.title]).then(() => {
+                this.$store.getters.getReports.data.forEach(element => {
+                    this.reports.push(element);
+                })
+            })
         }
     }
 }
--- resources/js/components/Search.vue
+++ resources/js/components/Search.vue
@@ -1,60 +1,59 @@
 <template>
-    <v-app>
-        <auth style="height: 80px"></auth>
-        <v-card>
-            <v-card-title>
-                <v-text-field label="Search" single-line solo style="padding-right: 20px" @change="getSearch()"
-                    v-model="title"></v-text-field>
-                <v-divider :vertical="true" color="black" class="mx-2"></v-divider>
-                <v-radio-group  v-model="category" @change="getSearch()" style="width: 400px; padding-left: 20px; padding-bottom: 20px;">
-                    <v-radio v-for="name in categories" :key="name" :label="name" :value="name"></v-radio>
-                </v-radio-group>
-            </v-card-title>
-            <v-card-text>
-                <div class="row justify-content-center">
-                    <div class="col-md-12" v-for="conference in conferences" :value="conference.id"
-                        :key="conference.id">
-                        <v-card elevation="3">
-                            <v-card-title>{{ conference.title }}</v-card-title>
-                            <v-card-text>
-                                Appointed time: {{ conference.date }} {{ conference.time }}
-                                <br>
-                                <span>
-                                    <v-btn x-big depressed color="success">
-                                        <router-link class="white--text" style="text-decoration: none; color: inherit;"
-                                            :to="{ name: 'ConferenceDetails', params: { id: conference.id } }" target="_blank">
-                                            More
-                                        </router-link>
-                                    </v-btn>
-                                </span>
-                            </v-card-text>
-                            Conference
-                        </v-card>
-                    </div>
+    <v-card>
+        <v-card-title>
+            <v-text-field label="Search" single-line solo style="padding-right: 20px" @change="getSearch()"
+                v-model="title"></v-text-field>
+            <v-divider :vertical="true" color="black" class="mx-2"></v-divider>
+            <v-radio-group v-model="category" @change="getSearch()"
+                style="width: 400px; padding-left: 20px; padding-bottom: 20px;">
+                <v-radio v-for="name in categories" :key="name" :label="name" :value="name"></v-radio>
+            </v-radio-group>
+        </v-card-title>
+        <v-card-text>
+            <div class="row justify-content-center">
+                <div class="col-md-12" v-for="conference in conferences" :value="conference.id" :key="conference.id">
+                    <v-card elevation="3">
+                        <v-card-title>{{ conference.title }}</v-card-title>
+                        <v-card-text>
+                            Appointed time: {{ conference.date }} {{ conference.time }}
+                            <br>
+                            <span>
+                                <v-btn x-big depressed color="success">
+                                    <router-link class="white--text" style="text-decoration: none; color: inherit;"
+                                        :to="{ name: 'ConferenceDetails', params: { id: conference.id } }"
+                                        target="_blank">
+                                        More
+                                    </router-link>
+                                </v-btn>
+                            </span>
+                        </v-card-text>
+                        Conference
+                    </v-card>
                 </div>
-                <div class="row justify-content-center">
-                    <div class="col-md-12" v-for="report in reports" :value="report.id" :key="report.id">
-                        <v-card elevation="3">
-                            <v-card-title>{{ report.thema }}</v-card-title>
-                            <v-card-text>
-                                Duration: {{ report.start_time }} to {{ report.end_time }}
-                                <br>
-                                <span>
-                                    <v-btn x-big depressed color="success">
-                                        <router-link class="white--text" style="text-decoration: none; color: inherit;"
-                                            :to="{ name: 'Details', params: { id: report.conference_id, rep_id: report.id } }" target="_blank">
-                                            More
-                                        </router-link>
-                                    </v-btn>
-                                </span>
-                            </v-card-text>
-                            Report
-                        </v-card>
-                    </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-12" v-for="report in reports" :value="report.id" :key="report.id">
+                    <v-card elevation="3">
+                        <v-card-title>{{ report.thema }}</v-card-title>
+                        <v-card-text>
+                            Duration: {{ report.start_time }} to {{ report.end_time }}
+                            <br>
+                            <span>
+                                <v-btn x-big depressed color="success">
+                                    <router-link class="white--text" style="text-decoration: none; color: inherit;"
+                                        :to="{ name: 'Details', params: { id: report.conference_id, rep_id: report.id } }"
+                                        target="_blank">
+                                        More
+                                    </router-link>
+                                </v-btn>
+                            </span>
+                        </v-card-text>
+                        Report
+                    </v-card>
                 </div>
-            </v-card-text>
-        </v-card>
-    </v-app>
+            </div>
+        </v-card-text>
+    </v-card>
 </template>
 
 <script>
@@ -67,17 +66,23 @@ export default {
         title: '',
     }),
     methods: {
-        getSearch(){
-            if(this.category == 'Conference'){
-                this.getConfByName()
+        getSearch() {
+            if (this.title != '') {
+                if (this.category == 'Conference') {
+                    this.getConfByName()
+                }
+                else if (this.category == 'Report') {
+                    this.getRepByName()
+                }
+                else {
+                    this.getConfByName()
+                    this.getRepByName()
+                }
             }
-            else if(this.category == 'Report'){
-                this.getRepByName()
-            }
-            else{
-                this.getConfByName()
-                this.getRepByName()
-            }
+        else{
+            this.conferences = []
+            this.reports = []
+        }
         },
         getConfByName(page = 1) {
             this.conferences = []
