ce50371c59f9dad583989e0d161e46f727d1cdda
Shizuco <93375450+Shizuco@users.noreply.github.com>
2022-10-20 11:09:48 +0300

    feat: create category test

--- tests/Feature/UserUpdateTest.php
+++ tests/Feature/UserUpdateTest.php
@@ -2,21 +2,66 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class UserUpdateTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    public function test_success_user_update()
     {
-        $response = $this->get('/');
+        $user = User::factory()->create();
+        Sanctum::actingAs(
+            $user,
+            ['*']
+        );
+        $response = $this->put('/api/user/change', [
+            'name' => 'fwefwef',
+            'surname' => 'fewfwef',
+            'birthday' => '2009-09-09',
+            'country' => 'Japan',
+            'email' => 'fwef@mfemwef.com',
+            'password' => '123123123',
+            'phone' => '380987823445',
+        ]);
+        $user->delete();
+        $response->assertStatus(201);
+    }
 
-        $response->assertStatus(200);
+    public function test_unauthenticate_user_update()
+    {
+        $user = User::factory()->create();
+        $response = $this->put('/api/user/change', [
+            'name' => 'fwefwef',
+            'surname' => 'fewfwef',
+            'birthday' => '2009-09-09',
+            'country' => 'Japan',
+            'email' => 'fwef@mfemwef.com',
+            'password' => '123123123',
+            'phone' => '380987823445',
+        ]);
+        $user->delete();
+        $response->assertStatus(302);
+    }
+
+    public function test_email_must_be_unique()
+    {
+        $user = User::factory()->create();
+        $userWithEmail = User::factory()->create();
+        Sanctum::actingAs(
+            $user,
+            ['*']
+        );
+        $response = $this->put('/api/user/change', [
+            'surname' => 'fewfwef',
+            'birthday' => '2009-09-09',
+            'country' => 'Japan',
+            'email' => $userWithEmail->email,
+            'password' => '123123123',
+            'phone' => '380987823445',
+        ]);
+        $user->delete();
+        $userWithEmail->delete();
+        $response->assertStatus(302);
     }
 }
